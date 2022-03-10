83a823f670b7f9c5d8bbac24d30bb6397896dccc
Edmund <eyu1@ualberta.ca>
2022-03-10 15:53:40 -0700

    API setup

--- cultivated_code/node-challenge-base/router/rentals.js
+++ cultivated_code/node-challenge-base/router/rentals.js
@@ -11,12 +11,19 @@ router.post("/", (req, res, err) => {
   const model = req.body.model;
   const start_date = req.body.start_date;
   const end_date = req.body.end_date;
+  const d1 = new Date(start_date);
+  const d2 = new Date(end_date);
 
   if (!name || !model) {
-    return res.status(422).send("Name or Model must not be empty!");
+    return res.status(422).json({ error: "Name or Model must not be empty." });
+  }
+  if (d1.valueOf() - d2.valueOf() >= 0) {
+    return res
+      .status(422)
+      .json({ error: "Start date must not be greater than the end date" });
   }
 
-  return res.status(200).send("sucess");
+  return res.status(200).json(req.body);
 });
 
 module.exports = { path: "/api/v1/rentals", router };
