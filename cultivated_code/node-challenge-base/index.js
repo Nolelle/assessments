74e61463d14bb320678fb2c119bc2e1b0cdf0fee
Edmund <eyu1@ualberta.ca>
2022-03-10 15:13:08 -0700

    intial post request setup

--- cultivated_code/node-challenge-base/index.js
+++ cultivated_code/node-challenge-base/index.js
@@ -1,34 +1,36 @@
 // Express instantiation
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 // Config
-const port = process.env.PORT || 3000
-const allRouters = require('./router/index')
-const createRoutes = require('./services/setup/createRoutes')
-const createDbConnection = require('./services/setup/createDbConnection')
-const requestLogFormat = ':method :url :status :res[content-length] - :response-time ms'
+const port = process.env.PORT || 3000;
+const allRouters = require("./router/index");
+const createRoutes = require("./services/setup/createRoutes");
+const createDbConnection = require("./services/setup/createDbConnection");
+const requestLogFormat =
+  ":method :url :status :res[content-length] - :response-time ms";
 
 const main = async () => {
   const db = createDbConnection();
-  app.use(morgan(requestLogFormat))
+  app.use(morgan(requestLogFormat));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use((req, res, next) => {
     req.db = db;
     next();
-  })
+  });
 
   // Application setup
-  app.get('/health', (req, res) => {
-    res.json({success: true})
-  })
+  app.get("/", (req, res) => {
+    res.json({ success: true });
+  });
 
-  createRoutes(app, allRouters)
+  createRoutes(app, allRouters);
 
   app.listen(port, () => {
-    console.log(`CultivatedCode car rental app listening on port: ${port}`)
-  })
-}
-
+    console.log(`CultivatedCode car rental app listening on port: ${port}`);
+  });
+};
 
 main();
--- cultivated_code/node-challenge-base/router/rentals.js
+++ cultivated_code/node-challenge-base/router/rentals.js
@@ -1,7 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/health', (req, res) => {
-  res.json({ success: true });
-})
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
 
-module.exports = { path: '/rentals', router };
+router.post("/", (req, res, err) => {
+  const name = req.body.name;
+  const model = req.body.model;
+  const start_date = req.body.start_date;
+  const end_date = req.body.end_date;
+
+  if (!name || !model) {
+    return res.status(422).send("Name or Model must not be empty!");
+  }
+
+  return res.status(200).send("sucess");
+});
+
+module.exports = { path: "/api/v1/rentals", router };
