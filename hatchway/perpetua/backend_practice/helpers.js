1ce7e88e30b288c1f5c0c62911b81bcc1b017590
Edmund <eyu1@ualberta.ca>
2022-03-13 19:00:49 -0600

    Work on back end practice

--- hatchway/perpetua/backend_practice/helpers.js
+++ hatchway/perpetua/backend_practice/helpers.js
@@ -1,5 +1,15 @@
-const findRecipeName = (recipeName) => {};
+const allRecipies = (data) => {
+  const recipies = [];
+  for (const recipes in data) {
+    for (const recipe of recipes) {
+      if (recipe.name) {
+        recipies.push(recipe.name);
+      }
+    }
+  }
+  return recipies;
+};
 
 module.exports = {
-  findRecipeName,
+  allRecipies,
 };
--- hatchway/perpetua/backend_practice/index.js
+++ hatchway/perpetua/backend_practice/index.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const PORT = 3000;
+const cors = require("cors");
+const PORT = 3000 || process.env.PORT;
 const { findRecipeName } = require("./helpers.js");
+const bodyParser = require("body-parser");
 
 //middleware
 app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
-app.get("/recipes", (req, res) => {
-  res.status(200).json({
-    recipeNames: ["scrambledEggs", "garlicPasta", "chai"],
-  });
+app.get("/recipes", async (req, res) => {
+  try {
+    res.status(200).json({
+      recipeNames: ["scrambledEggs", "garlicPasta", "chai"],
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-app.get("/recipes/details/:recipe", (rec, res) => {
-  const recipeName = req.params.recipe;
-
-  res.status(200).json({});
+app.get("/recipes/details/:recipe", async (rec, res) => {
+  try {
+    const { recipeName } = req.params;
+    res.status(200).json({});
+  } catch (err) {
+    console.err(err.message);
+  }
 });
 
 app.listen(PORT, () => {
