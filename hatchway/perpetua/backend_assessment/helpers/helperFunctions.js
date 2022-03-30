016e41d0ae9376d3cacc142b535e6c0f0cd6fe78
Edmund <eyu1@ualberta.ca>
2022-03-30 17:14:39 -0600

    working on backend assessment

--- hatchway/perpetua/backend_assessment/helpers/helperFunctions.js
+++ hatchway/perpetua/backend_assessment/helpers/helperFunctions.js
@@ -1,23 +1,33 @@
 const axios = require("axios");
-const getPosts = async (tag) => {
+const { request } = require("express");
+const getPosts = async (tag, sortBy, direction) => {
   try {
     const tagsArray = tag.split(",");
-    const requests = tagsArray.map((tag) => {
-      axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`);
-    });
+    const requests = tagsArray.map((tag) =>
+      axios.get(
+        `https://api.hatchways.io/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`
+      )
+    );
     const results = await Promise.all(requests);
-    return results;
+    const posts = results.map((ele) => ele.data.posts);
+    const filteredPosts = [];
+    const filteredPostsID = [];
+    posts.forEach((ele) => {
+      ele.forEach((ele2) => {
+        if (!filteredPostsID.includes(ele2.id)) {
+          filteredPosts.push(ele2);
+          filteredPostsID.push(ele2.id);
+        }
+      });
+    });
+    console.log(filteredPosts);
+    return filteredPosts;
   } catch (err) {
     console.error(err.message);
   }
 };
 
-const tags = "tech,health,science";
-
-getPosts(tags)
-  .then((data) => console.log(data))
-  .catch((err) => {
-    console.log(err.message);
-  });
+const tags = "history,tech";
+const answer = getPosts(tags);
 
 module.exports = { getPosts };
